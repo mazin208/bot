4eb8875ae8e56233d3b6a78549179bfccbddf084
mazin208 <eman135744@gmail.com>
2021-02-19 13:54:47 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,34 @@ def send_welc(message):
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
   bot.send_message(message.chat.id, "Welcome user")
+#Intitialize YouTube downloader
+ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
+# works when /ytdl <link> is given
+@bot.message_handler(commands=['ytdl'])
+def down(msg):
+    args = msg.text.split()[1]
+    try:
+        with ydl:
+            result = ydl.extract_info(
+                args,
+                download=False  # We just want to extract the info
+            )
+
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+        else:
+            # Just a video
+            video = result
+        
+        for i in video['formats']:
+            link = '<a href=\"' + i['url'] + '\">' + 'link' + '</a>'
+            if i.get('format_note'):
+                bot.reply_to(msg, 'Quality-' + i['format_note'] + ': ' + link, parse_mode='HTML')
+            else:
+                bot.reply_to(msg, link, parse_mode='HTML', disable_notification=True)
+    except:
+        bot.reply_to(msg, 'This can\'t be downloaded by me')
 #pool~start the bot
 bot.polling()
