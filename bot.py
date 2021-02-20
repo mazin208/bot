aab30f65ce852f5ee02ba5b63fea6163fc41085d
mazin208 <eman135744@gmail.com>
2021-02-20 05:48:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
-#required packages
-import telebot
-import requests
-import os
-import json
-from time import sleep
-import youtube_dl
-from http.server import BaseHTTPRequestHandler, HTTPServer
+try:
+    # Python 2
+    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+except ImportError:
+    # Python 3
+    from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import logging
 import ssl
 
-telegramToken = "telgram"
+import telebot
 
+API_TOKEN = '<api_token>'
 
 WEBHOOK_HOST = 'makuzo.herokuapp.com'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
@@ -20,14 +19,21 @@ WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (telgramToken)
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
 
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 
-bot = telebot.TeleBot(telegramToken)
+bot = telebot.TeleBot(API_TOKEN)
 
 
 # WebhookHandler, process webhook calls
@@ -57,20 +63,30 @@ class WebhookHandler(BaseHTTPRequestHandler):
         else:
             self.send_error(403)
             self.end_headers()
-            
 
-#handling /commands
-@bot.message_handler(commands=['motivate'])
-def send_welc(message):
-        quote = requests.request(url='https://api.quotable.io/random',method='get')
-        bot.send_message(message.chat.id, quote.json()['content'])
 
-@bot.message_handler(commands=['start', 'help'])
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-  bot.send_message(message.chat.id, "Welcome user")
-  
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
-print(x)
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+#bot.remove_webhook()
+
+# Set webhook
+# Beacuse telegram bot api server will check webhook server is alive.
+# Here we need set webhook after server started manually.
+#bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+#                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start server
 httpd = HTTPServer((WEBHOOK_LISTEN, WEBHOOK_PORT),
