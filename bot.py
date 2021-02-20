5c1471bc9187cd68b5dc86b0bf2e57ad86816af1
mazin208 <eman135744@gmail.com>
2021-02-20 06:09:21 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,58 +16,16 @@ import os
 
 API_TOKEN = '<api_token>'
 
-WEBHOOK_HOST = 'makuzo.herokuapp.com'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
-
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
-
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
+# add handlers
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://makuzo.herokuapp.com/" + TOKEN)
+updater.idle()
 
 bot = telebot.TeleBot(API_TOKEN)
-
-
-# WebhookHandler, process webhook calls
-class WebhookHandler(BaseHTTPRequestHandler):
-    server_version = "WebhookHandler/1.0"
-
-    def do_HEAD(self):
-        self.send_response(200)
-        self.end_headers()
-
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-
-    def do_POST(self):
-        if self.path == WEBHOOK_URL_PATH and \
-           'content-type' in self.headers and \
-           'content-length' in self.headers and \
-           self.headers['content-type'] == 'application/json':
-            json_string = self.rfile.read(int(self.headers['content-length']))
-
-            self.send_response(200)
-            self.end_headers()
-
-            update = telebot.types.Update.de_json(json_string)
-            bot.process_new_messages([update.message])
-        else:
-            self.send_error(403)
-            self.end_headers()
-
 
 
 x = bot.get_me()
@@ -109,23 +67,3 @@ def send_video(message):
 
 
 print(x)
-
-# Remove webhook, it fails sometimes the set if there is a previous webhook
-#bot.remove_webhook()
-
-# Set webhook
-# Beacuse telegram bot api server will check webhook server is alive.
-# Here we need set webhook after server started manually.
-#bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-#                certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Start server
-httpd = HTTPServer((WEBHOOK_LISTEN, WEBHOOK_PORT),
-                   WebhookHandler)
-
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               certfile=WEBHOOK_SSL_CERT,
-                               keyfile=WEBHOOK_SSL_PRIV,
-                               server_side=True)
-
-httpd.serve_forever()
