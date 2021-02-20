96832a3ac2a0dbcebba8c5af52d8209b1b4a5708
mazin208 <eman135744@gmail.com>
2021-02-20 05:07:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,83 @@
 import telebot
 import requests
 import os
-import json#Config vars
-with open("config.json") as f:
- token = json.load(f)#initialise bot
-bot = telebot.TeleBot(token["telegramToken"])
-x = bot.get_me()
+import json
+from time import sleep
+import youtube_dl
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-#handling commands - /start
-@bot.message_handler(commands=["start", "help"])
+import logging
+import ssl
+
+telegramToken = "telgram"
+
+
+WEBHOOK_HOST = 'makuzo.herokuapp.com'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (telgramToken)
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(telegramToken)
+
+
+# WebhookHandler, process webhook calls
+class WebhookHandler(BaseHTTPRequestHandler):
+    server_version = "WebhookHandler/1.0"
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+
+    def do_POST(self):
+        if self.path == WEBHOOK_URL_PATH and \
+           'content-type' in self.headers and \
+           'content-length' in self.headers and \
+           self.headers['content-type'] == 'application/json':
+            json_string = self.rfile.read(int(self.headers['content-length']))
+
+            self.send_response(200)
+            self.end_headers()
+
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_messages([update.message])
+        else:
+            self.send_error(403)
+            self.end_headers()
+            
+
+#handling /commands
+@bot.message_handler(commands=['motivate'])
+def send_welc(message):
+        quote = requests.request(url='https://api.quotable.io/random',method='get')
+        bot.send_message(message.chat.id, quote.json()['content'])
+
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
- bot.send_message(message.chat.id, "Welcome user")
- 
+  bot.send_message(message.chat.id, "Welcome user")
+  
 
-#handling commands - /motivate
-@bot.message_handler(commands=["motivate"])
-def send_quotes(message):
- quote= requests.request(url="https://api.quotable.io/random",method='get')
- bot.reply_to(message, quote.json()["content"]) 
- 
-print(x)#pool~start the bot
-bot.polling()
+print(x)
+
+# Start server
+httpd = HTTPServer((WEBHOOK_LISTEN, WEBHOOK_PORT),
+                   WebhookHandler)
+
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               certfile=WEBHOOK_SSL_CERT,
+                               keyfile=WEBHOOK_SSL_PRIV,
+                               server_side=True)
+
+httpd.serve_forever()
