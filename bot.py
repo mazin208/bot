ebecac26a4c27c4651693d4e903c30e8f22e8b2d
mazin208 <eman135744@gmail.com>
2021-02-21 04:25:22 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,69 +1,151 @@
-try:
-    # Python 2
-    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-except ImportError:
-    # Python 3
-    from http.server import BaseHTTPRequestHandler, HTTPServer
-
-import logging
-import ssl
-import pafy
 import telebot
-from telebot import types
-from tqdm import tqdm 
 import requests
 import os
+import json
+#Config vars
 
-API_TOKEN = '<api_token>'
+import logging
 
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://makuzo.herokuapp.com/" + TOKEN)
-updater.idle()
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
-bot = telebot.TeleBot(API_TOKEN)
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
 
+logger = logging.getLogger(__name__)
 
-x = bot.get_me()
-
-#handling commands - /start
-@bot.message_handler(commands=["start", "help"])
-def send_welcome(message):
- bot.send_message(message.chat.id, "Welcome user")
-
- 
- 
- 
- #markup = types.ForceReply(selective=False)
- #markup = types.ReplyKeyboardMarkup()
- #itembtna = types.KeyboardButton('a')
- #itembtnv = types.KeyboardButton('v')
- #itembtnc = types.KeyboardButton('c')
- #itembtnd = types.KeyboardButton('d')
- #itembtne = types.KeyboardButton('e')
- #markup.row(itembtna, itembtnv)
- #markup.row(itembtnc, itembtnd, itembtne)
- #bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
- 
-
-#handling commands - /motivate
-@bot.message_handler(commands=["motivate"])
-def send_quotes(message):
- quote= requests.request(url="https://api.quotable.io/random",method='get')
- bot.reply_to(message, quote.json()["content"]) 
- 
-@bot.message_handler(commands=["down"])
-def send_video(message):
- markup = types.ForceReply(selective=False)
- url = bot.send_message(message.chat.id, "Send me the URL:", reply_markup=markup)
- myvid = pafy.get_playlist(url)
- bot.reply_to(message, str(myvid))
- for i in tqdm (range (100), desc="Loading..."): 
-    pass
+GENDER, PHOTO, LOCATION, BIO = range(4)
 
 
-print(x)
+def start(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Are you a boy or a girl?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+
+    return GENDER
+
+
+def gender(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'I see! Please send me a photo of yourself, '
+        'so I know what you look like, or send /skip if you don\'t want to.',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+def photo(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    photo_file = update.message.photo[-1].get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text(
+        'Gorgeous! Now, send me your location please, ' 'or send /skip if you don\'t want to.'
+    )
+
+    return LOCATION
+
+
+def skip_photo(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text(
+        'I bet you look great! Now, send me your location please, ' 'or send /skip.'
+    )
+
+    return LOCATION
+
+
+def location(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    update.message.reply_text(
+        'Maybe I can visit you sometime! ' 'At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def skip_location(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text(
+        'You seem a bit paranoid! ' 'At last, tell me something about yourself.'
+    )
+
+    return BIO
+
+
+def bio(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("1686921032:AAH7CKmaHFi8KGNKa8rgdm4tRFwUPB6sRY4")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
+            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
+            LOCATION: [
+                MessageHandler(Filters.location, location),
+                CommandHandler('skip', skip_location),
+            ],
+            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
